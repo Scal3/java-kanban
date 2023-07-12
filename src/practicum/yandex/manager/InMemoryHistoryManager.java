c1d8da64be613eebab3b832e2c48487025757f4c
Herman <cool.goga501@gmail.com>
2023-07-12 14:16:12 +0200

    fix/rebuild InMemoryHistoryManager logic

--- src/practicum/yandex/manager/InMemoryHistoryManager.java
+++ src/practicum/yandex/manager/InMemoryHistoryManager.java
@@ -3,39 +3,30 @@ package practicum.yandex.manager;
 import practicum.yandex.task.Task;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final byte MAX_HISTORY_QUANTITY = 10;
-    private final Map<Integer, Task> tasksHistory;
-    private int historyTasksCounter;
+    private final List<Task> tasksHistory;
 
     public InMemoryHistoryManager() {
-        tasksHistory = new HashMap<>();
-        historyTasksCounter = 0;
+        tasksHistory = new ArrayList<>();
     }
 
     @Override
     public void add(Task task) {
-        tasksHistory.put(++historyTasksCounter, task);
+        tasksHistory.remove(task);
+
+        if (tasksHistory.size() < MAX_HISTORY_QUANTITY) {
+            tasksHistory.add(task);
+        } else {
+            tasksHistory.remove(0);
+            tasksHistory.add(task);
+        }
     }
 
     @Override
     public List<Task> getHistory() {
-        List<Task> history = new ArrayList<>();
-
-        if (historyTasksCounter <= MAX_HISTORY_QUANTITY) {
-            for (int i = historyTasksCounter; i > 0; i--) {
-                history.add(tasksHistory.get(i));
-            }
-        } else {
-            for (int i = historyTasksCounter; i > historyTasksCounter - MAX_HISTORY_QUANTITY; i--) {
-                history.add(tasksHistory.get(i));
-            }
-        }
-
-        return history;
+        return new ArrayList<>(tasksHistory);
     }
 }
